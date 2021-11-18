bfd4727fb163e92f8d5a0664fd6035e47b048d3a
Artur Przychodzen <artur2p@gmail.com>
2021-11-18 17:39:01 +0100

    inint

---
+++ src/main/java/io/artur/spring/securityoidc/SecurityOidcApplication.java
@@ -0,0 +1,13 @@
+package io.artur.spring.securityoidc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityOidcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityOidcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/artur/spring/securityoidc/SecurityOidcApplicationTests.java
@@ -0,0 +1,13 @@
+package io.artur.spring.securityoidc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityOidcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
